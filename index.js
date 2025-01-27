2120ec2891028f9a854fb0d033c35f4c712aa2de
CallMeVinn <vinndeveloper@gmail.com>
2025-01-28 01:36:01 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ process.on("uncaughtException", (error) => console.info(error));
 
 const { Client } = require('whatsapp-web.js');
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
 
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
