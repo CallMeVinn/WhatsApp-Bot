a01cabeb84393b577b4dd274d9e58030c0a63360
CallMeVinn <vinndeveloper@gmail.com>
2025-01-28 02:43:03 +0700

    update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const webhook = new WebhookClient({ url: "https://discord.com/api/webhooks/13335
 const qrcode = require("qrcode");
 
 client.on('qr', async(qr) => {
-    const data = await qrcode.toCanvas(qr);
+    const data = await qrcode.toDataURL(qr);
     
     console.log("✅ | QR Received");
-    webhook.send({ files: [{ attachment: data, name: "QR-WhatsApp.png" }] });
+    webhook.send({ files: [{ attachment: Buffer.from(data).toString('base64'), name: "QR-WhatsApp.png" }] });
 });
 
 client.on('ready', () => {
